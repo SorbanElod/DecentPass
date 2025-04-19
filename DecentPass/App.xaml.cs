ce5624c7fc3a9fa1cbc1217deebe0ea3e95d15d6
SorbanElod <sorbanelod03@gmail.com>
2025-04-19 11:33:40 +0300

    init .net MAUI app

---
+++ DecentPass/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace DecentPass
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
